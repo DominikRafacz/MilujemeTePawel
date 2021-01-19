120ffc0dd37cfad504c01236ec64c3a214be5a70
pysiakk <karol.pysiak.13@gmail.com>
2021-01-19 21:28:37 +0100

    Changed knowledge base to YAGO

--- test.py
+++ test.py
@@ -1,17 +1,22 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+sparql = SPARQLWrapper("https://yago-knowledge.org/sparql/query")
 sparql.setQuery("""
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX schema: <http://schema.org/>
     SELECT ?item ?prop ?val
-    WHERE 
+    WHERE
     {
-    ?item wdt:P31 wd:Q7366;
-       ?prop ?val . }
+    ?item rdf:type schema:MusicComposition ;
+        ?prop ?val . }
     LIMIT 100
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
+
+# print(json.dumps(results["results"]["bindings"], indent=4))
+
 
 print(len(results["results"]["bindings"]))
 
@@ -21,7 +26,7 @@ for result in results["results"]["bindings"]:
         tmp[result['item']['value']]
     except (KeyError) as e:
         tmp[result['item']['value']] = []
-        
+
     tmp[result['item']['value']].append({"prop":result['prop'], "val": result['val']})
 
 print(json.dumps(tmp, indent=4))
