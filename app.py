0a8500e39a9939e89333cd10f4cc73c17e51e274
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-01-23 12:28:34 +0100

    added more endpoints and redirecting

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from util import load_chosen_properties
 
 app = Flask(__name__)
 
-fields = ['label', 'comment', 'alternateName', 'datePublished', 'composer', 'isPartOf']
+fields = load_chosen_properties()
 
 
-@app.route('/form')
+def metrics_for_query(query_params):
+    return {'some_result': None}
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('form'))
+
+
+@app.route('/form', methods=('GET', 'POST'))
 def form():
+    if request.method == 'POST':
+        query_params = {field: request.form[field] for field in fields}
+        metrics_results = metrics_for_query(query_params)
+        return redirect(url_for('results', metrics_results=metrics_results))
     return render_template('form.html.jinja2', fields=fields)
+
+
+@app.route('/results/<metrics_results>')
+def results(metrics_results):
+    return render_template('results.html.jinja2', metrics_results=metrics_results)
 
 
 if __name__ == '__main__':
