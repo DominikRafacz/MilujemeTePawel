e5b66d5a2469c771668a6dbc751cac49e65df35f
pysiakk <karol.pysiak.13@gmail.com>
2021-01-23 21:05:13 +0100

    Added example usages with rdflib

--- querying.py
+++ querying.py
@@ -1,5 +1,9 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import dateutil.parser as parser
+from rdflib.plugins.stores.sparqlstore import SPARQLStore
+from rdflib import Namespace
+from rdflib.namespace import FOAF, RDF, RDFS, XSD
+from rdflib.term import Literal, URIRef
 
 
 def reformat_results(results):
@@ -37,3 +41,55 @@ def mock_query():
     """)
     sparql.setReturnFormat(JSON)
     return reformat_results(sparql.query().convert())
+
+
+def get_namespaces():
+    schema = Namespace("http://schema.org/")
+    ns = dict(rdf=RDF, foaf=FOAF, rdfs=RDFS, xsd=XSD, schema=schema)
+    return ns
+
+
+def rdf_query():
+
+    namespaces = get_namespaces()
+    sparql_store = SPARQLStore("https://yago-knowledge.org/sparql/query")
+
+    query_string = """
+    SELECT ?item ?prop ?val
+    WHERE
+    {
+        ?item rdf:type schema:MusicComposition ;
+              ?prop ?val . 
+        FILTER(?prop IN (<http://www.w3.org/2000/01/rdf-schema#label>, <http://www.w3.org/2000/01/rdf-schema#comment>, 
+        schema:alternateName, schema:datePublished, schema:composer, schema:isPartOf, schema:genre, schema:inLanguage, 
+        schema:lyricist, schema:producer, schema:dateCreated, schema:author, schema:isBasedOn, schema:about, 
+        schema:award, schema:creator, schema:contributor, schema:publisher, schema:translator))
+    }
+    LIMIT 1000
+    """
+
+    result = sparql_store.query(query_string, initNs=namespaces)
+    # for row in list(result):
+    #     print(row)
+    return result
+
+
+def reformat_results_rdf(results):
+    ret = {}
+    for result in list(results):
+        try:
+            ret[str(result[0])]
+        except KeyError:
+            ret[str(result[0])] = []
+
+        if str(result[1]) in ("http://schema.org/dateCreated", "http://schema.org/datePublished"):
+            val = parser.parse(str(result[2])).year
+        else:
+            val = result[2]
+        print(val)
+        ret[str(result[0])].append({"prop": str(result[1]), "val": val})
+    return ret
+
+
+# results = rdf_query()
+# response = reformat_results_rdf(results)
--- scoring.py
+++ scoring.py
@@ -24,6 +24,28 @@ def match_properties(obj_first, obj_second):
             matched[key]['second'].append(i)
     return matched, mismatched
 
+def match_properties_rdflib(obj_first, obj_second):
+    keys_first = {obj_first[i]['prop'] for i in range(len(obj_first))}
+    keys_second = {obj_second[i]['prop'] for i in range(len(obj_second))}
+    matched_keys = keys_first.intersection(keys_second)
+    mismatched_keys_first = keys_first - matched_keys
+    mismatched_keys_second = keys_second - matched_keys
+    matched = {}
+    mismatched = {}
+    for key in matched_keys:
+        matched[key] = {'first': [], 'second': []}
+    for key in mismatched_keys_first | mismatched_keys_second:
+        mismatched[key] = None
+    for i in range(len(obj_first)):
+        key = obj_first[i]['prop']
+        if key in matched_keys:
+            matched[key]['first'].append(i)
+    for i in range(len(obj_second)):
+        key = obj_second[i]['prop']
+        if key in matched_keys:
+            matched[key]['second'].append(i)
+    return matched, mismatched
+
 
 def linear_dist(x, y):
     return abs(int(x) - int(y))
@@ -39,6 +61,15 @@ def iterate_over_key(obj_first, obj_second, matched, key, dist_function):
                 lowest_distance = dist
     return lowest_distance
 
+def iterate_over_key_rdflib(obj_first, obj_second, matched, key, dist_function):
+    lowest_distance = sys.maxsize
+    for pos_match_first in matched[key]['first']:
+        for pos_match_second in matched[key]['second']:
+            dist = dist_function(obj_first[pos_match_first]['val'],
+                                 obj_second[pos_match_second]['val'])
+            if dist < lowest_distance:
+                lowest_distance = dist
+    return lowest_distance
 
 def calculate_score(obj_first, obj_second):
     matched, mismatched = match_properties(obj_first, obj_second)
@@ -48,6 +79,19 @@ def calculate_score(obj_first, obj_second):
             lowest_distance = iterate_over_key(obj_first, obj_second, matched, key, linear_dist)
         else:
             lowest_distance = iterate_over_key(obj_first, obj_second, matched, key, Levenshtein.distance)
+        scores[key] = 1 / (1 + lowest_distance)
+    score = sum(scores.values()) / (len(matched) + len(mismatched) / 2)
+    return {'total_score': score, 'partial_scores': scores}
+
+
+def calculate_score_rdflib(obj_first, obj_second):
+    matched, mismatched = match_properties_rdflib(obj_first, obj_second)
+    scores = {}
+    for key in matched.keys():
+        if key in ("http://schema.org/datePublished", "http://schema.org/dateCreated"):
+            lowest_distance = iterate_over_key_rdflib(obj_first, obj_second, matched, key, linear_dist)
+        else:
+            lowest_distance = iterate_over_key_rdflib(obj_first, obj_second, matched, key, Levenshtein.distance)
         scores[key] = 1 / (1 + lowest_distance)
     score = sum(scores.values()) / (len(matched) + len(mismatched) / 2)
     return {'total_score': score, 'partial_scores': scores}
