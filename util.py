ce97189462e7801d2e641e1fcf02f4d23289e099
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-01-23 13:26:30 +0100

    enforced order

--- util.py
+++ util.py
@@ -2,6 +2,6 @@ import json
 
 
 def load_chosen_properties():
-    with open('chosen_properties.json') as file:
+    with open('data/chosen_properties.json') as file:
         chosen = json.load(file)
         return list(chosen.keys())
