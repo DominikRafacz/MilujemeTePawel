94d530272059b0a8a005afc631ce3b0ccbc4b2dc
pysiakk <karol.pysiak.13@gmail.com>
2021-01-24 14:37:58 +0100

    First version of filter

--- YAGOTemplater/querying.py
+++ YAGOTemplater/querying.py
@@ -2,17 +2,47 @@ import dateutil.parser as parser
 from rdflib import Namespace
 from rdflib.namespace import FOAF, RDF, RDFS, XSD
 from rdflib.plugins.stores.sparqlstore import SPARQLStore
-from YAGOTemplater.util import EmptyFormException
-
-
-def check_form_params(form_params):
-    if all(map(lambda key: (form_params['props'][key] == ''), form_params['props'].keys())):
-        raise EmptyFormException('Form has no field filled')
+# from YAGOTemplater.util import EmptyFormException
+#
+#
+# def check_form_params(form_params):
+#     if all(map(lambda key: (form_params['props'][key] == ''), form_params['props'].keys())):
+#         raise EmptyFormException('Form has no field filled')
 
 
 # mock
 def prepare_filter_string(form_params):
-    return ''
+    parms_getters = {
+        "http://schema.org/datePublished": "?item <http://schema.org/datePublished> ?date .",
+        "http://schema.org/composer": "?item <http://schema.org/composer> ?composer .",
+        "http://schema.org/isPartOf": "?item <http://schema.org/isPartOf> ?isPartOf .",
+        "http://schema.org/genre": "?item <http://schema.org/genre> ?genre .",
+        "http://schema.org/inLanguage": "?item <http://schema.org/inLanguage> ?inLanguage .",
+        "http://schema.org/author": "?item <http://schema.org/author> ?author ."
+    }
+    upper = ''
+    lower = ''
+    for key in list(form_params.keys()):
+        upper += parms_getters[key] + "\n"
+        if key == "http://schema.org/datePublished":
+            if form_params["http://schema.org/datePublished"]["from"] is not None:
+                lower += "FILTER(year(?date) >= year(" + form_params["http://schema.org/datePublished"]["from"] \
+                         + "^^xsd:date)) .\n"
+            if form_params["http://schema.org/datePublished"]["to"] is not None:
+                lower += "FILTER(year(?date) <= year(" + form_params["http://schema.org/datePublished"]["to"] \
+                         + "^^xsd:date)) .\n"
+        elif key == "http://schema.org/composer":
+            lower += "FILTER(?composer = <" + form_params["http://schema.org/composer"] + "> .\n"
+        elif key == "http://schema.org/isPartOf":
+            lower += "FILTER(?isPartOf = <" + form_params["http://schema.org/isPartOf"] + "> .\n"
+        elif key == "http://schema.org/genre":
+            lower += "FILTER(?genre = <" + form_params["http://schema.org/genre"] + "> .\n"
+        elif key == "http://schema.org/inLanguage":
+            lower += "FILTER(?inLanguage = <" + form_params["http://schema.org/inLanguage"] + "> .\n"
+        elif key == "http://schema.org/author":
+            lower += "FILTER(?author = <" + form_params["http://schema.org/author"] + "> .\n"
+
+    return upper + lower
 
 
 def get_chosen_properties_filter():
@@ -24,7 +54,7 @@ def get_chosen_properties_filter():
     """
 
 
-def prepare_queery(form_params):
+def prepare_query(form_params):
     query_string = """
             SELECT ?item ?prop ?val
             WHERE {
